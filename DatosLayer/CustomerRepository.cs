c41ea9acf6d9d9d7394377bd1718a6a2d21d6b55
Elizabeth Abrego <an0174032023@unab.edu.sv>
2024-08-24 20:31:28 -0600

    Guía XVI, finalizada.

--- DatosLayer/CustomerRepository.cs
+++ DatosLayer/CustomerRepository.cs
@@ -66,11 +66,12 @@ namespace DatosLayer
                 selectForID = selectForID + "      ,[Phone] " + "\n";
                 selectForID = selectForID + "      ,[Fax] " + "\n";
                 selectForID = selectForID + "  FROM [dbo].[Customers] " + "\n";
-                selectForID = selectForID + $"  Where CustomerID = '{id}'";
+                selectForID = selectForID + $"  Where CustomerID = @customerId ";
 
-
+            
                 using (SqlCommand comando = new SqlCommand(selectForID, conexion))
                 {
+                    comando.Parameters.AddWithValue("customerId", id);
                     var reader = comando.ExecuteReader();
                     customers customers = null;
                     //validadmos 
